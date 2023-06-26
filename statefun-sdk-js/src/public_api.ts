333170d44b36603f2f3b3ae29d578fa67dbb7530
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2023-06-26 19:34:06 +0900

    Remove unnecessary files for our SDK

--- statefun-sdk-js/src/public_api.ts
+++
@@ -1,5 +0,0 @@
-export {Context} from "./context";
-export {FunctionOpts, ValueSpecOpts, Address, Type} from "./core";
-export {kafkaEgressMessage, kinesisEgressMessage} from "./egress";
-export {Message, messageBuilder, egressMessageBuilder} from "./message";
-export {StateFun} from "./statefun";
